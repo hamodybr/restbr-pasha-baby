ff4dd6f890897d50c5a7a156c4d685e7f533c149
HaMoDyBr <78080184+hamodybr@users.noreply.github.com>
2026-09-17 16:31:41 +0300

    Bump product gallery hotfix asset

--- js/pasha-arabic-only.js
+++ js/pasha-arabic-only.js
@@ -235,7 +235,7 @@
 
     loadScript('pashaOrderColorBridgeScript', 'js/pasha-order-color-bridge.js?v=1.1');
     loadScript('pashaColorImageGalleryScript', 'js/pasha-color-image-gallery.js?v=2.1');
-    loadScript('pashaProductGalleryThermalScript', 'js/pasha-product-gallery-thermal-v1.js?v=1.0');
+    loadScript('pashaProductGalleryThermalScript', 'js/pasha-product-gallery-thermal-v1.js?v=1.1');
     loadScript('pashaOrderSubmitScript', 'js/pasha-order-submit.js?v=2.0');
     loadScript('pashaArabicNewsTickerScript', 'js/arabic-news-ticker.js?v=1.1');
     installStorefrontPerformanceGuards();
